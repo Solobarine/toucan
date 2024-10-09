11f067a3398bc2e0d1d940f036f03e0a248521b7
Solly <solobarine@gmail.com>
2024-10-09 14:15:30 +0100

    feat:
    - Add Header
    - Update style
    - Add Sidebar toggle

--- client/src/layouts/main.tsx
+++ client/src/layouts/main.tsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "../components/footer";
 import SideBar from "../components/sideBar";
 import { AppDispatch, RootState } from "../features/store";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useRef } from "react";
-import { setSideBarState } from "../features/slices/settings";
+import { setSideBarState, toggleSideBar } from "../features/slices/settings";
+import SmallAvatar from "../components/avatar/small";
 
 const Main = () => {
-  const { isDarkTheme } = useSelector((state: RootState) => state.settings);
+  const { isDarkTheme, isSideBarOpen } = useSelector(
+    (state: RootState) => state.settings
+  );
   // Set Theme
   useEffect(() => {
     if (isDarkTheme) {
@@ -51,6 +54,8 @@ const Main = () => {
     touchEndX.current = null;
   };
 
+  const navigate = useNavigate();
+
   return (
     <section className="main font-itim bg-light dark:bg-dark text-gray-800 dark:text-gray-100">
       <div className="flex items-start min-h-svh">
@@ -61,6 +66,29 @@ const Main = () => {
           onTouchMove={handleTouchMove}
           onTouchEnd={handleTouchEnd}
         >
+          <div className="sticky top-0 bg-white dark:bg-stone-900 py-2 px-4 flex items-center justify-between">
+            <SmallAvatar src="/favicon-32x32.png" alt="Toucan Logo" />
+            <div className="text-2xl text-gray-600 dark:text-gray-200 flex items-center gap-2">
+              <button
+                className="w-6 h-6 grid place-items-center"
+                onClick={() => navigate("/settings")}
+              >
+                <i className="bx bx-cog" />
+              </button>
+              <button
+                className="w-6 h-6 grid place-items-center"
+                onClick={() => navigate("/notifications")}
+              >
+                <i className="bx bx-bell" />
+              </button>
+              <button
+                className="sm:hidden w-6 h-6 grid place-items-center"
+                onClick={() => dispatch(toggleSideBar())}
+              >
+                <i className={isSideBarOpen ? "bx bx-x" : "bx bx-menu"} />
+              </button>
+            </div>
+          </div>
           <Outlet />
         </section>
       </div>
