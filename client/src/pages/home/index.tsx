c8251a96c77086650a219a17d5aae03553277c5d
Solly <solobarine@gmail.com>
2024-10-10 12:45:12 +0100

    Add Home Section

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,12 +1,20 @@
 import { Helmet } from "react-helmet";
+import Hero from "../../components/home/hero";
+import Features from "../../components/home/features";
+import Benefits from "../../components/home/benefits";
+import Reviews from "../../components/home/reviews";
 
 const Home = () => {
   return (
-    <section>
+    <div className="bg-gradient-to-r from-primary via-purple-500 to-pink-500 min-h-screen">
       <Helmet>
         <title>Toucan - Connecting You, Effortlessly and Vibrantly!</title>
       </Helmet>
-    </section>
+      <Hero />
+      <Features />
+      <Benefits />
+      <Reviews />
+    </div>
   );
 };
 
