cbc518b96fe9cbc111660412904ed3e429c155ca
Solly <solobarine@gmail.com>
2024-10-23 23:49:11 +0100

    feature: implement authentication for users.

---
+++ backend/priv/repo/migrations/20241012102813_create_users.exs
@@ -0,0 +1,17 @@
+defmodule Backend.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :first_name, :string, null: false
+      add :last_name, :string, null: false
+      add :username, :string, null: false
+      add :email, :string, null: false
+      add :password_hash, :string, null: false
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create unique_index(:users, [:email])
+  end
+end
