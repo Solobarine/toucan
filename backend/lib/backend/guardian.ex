67c431094962abafce5bc233a60145bef3445691
Solly <solobarine@gmail.com>
2024-10-24 12:53:09 +0100

    auth: modify token generation and verification

--- backend/lib/backend/guardian.ex
+++ backend/lib/backend/guardian.ex
@@ -1,5 +1,6 @@
 defmodule Backend.Guardian do
   use Guardian, otp_app: :backend
+  alias Backend.Accounts
 
   def subject_for_token(%{id: id}, _claims) do
     # You can use any value for the subject of your token but
@@ -18,10 +19,15 @@ defmodule Backend.Guardian do
     # Here we'll look up our resource from the claims, the subject can be
     # found in the `"sub"` key. In above `subject_for_token/2` we returned
     # the resource id so here we'll rely on that to look it up.
-    resource = Backend.get_resource_by_id(id)
-    {:ok,  resource}
+     resource = Accounts.get_user!(id)
+     {:ok,  resource}
   end
   def resource_from_claims(_claims) do
     {:error, :reason_for_error}
+  end
+
+  def generate_jwt(user) do
+    {:ok, token, _claims} = encode_and_sign(user)
+    token
   end
 end
--- backend/lib/backend_web/controllers/auth_controller.ex
+++ backend/lib/backend_web/controllers/auth_controller.ex
@@ -10,10 +10,10 @@ defmodule BackendWeb.AuthController do
   @doc """
   Register User
   """
-  def register(conn, %{"first_name" => first_name, "last_name" => last_name, "username" => username,"email" => email, "password" => password}) do
-    case Accounts.register(%{"first_name" => first_name, "last_name" => last_name, "username" => username,"email" => email, "password" => password}) do
+  def register(conn, %{"first_name" => first_name, "last_name" => last_name, "username" => username,"email" => email, "password_hash" => password}) do
+    case Accounts.register(%{"first_name" => first_name, "last_name" => last_name, "username" => username,"email" => email, "password_hash" => password}) do
       {:ok, user} ->
-        token = Accounts.generate_jwt(user)
+        token = Guardian.generate_jwt(user)
         json(conn, %{token: token})
 
       {:error, changeset} ->
@@ -29,13 +29,25 @@ defmodule BackendWeb.AuthController do
   def login(conn, %{"email" => email, "password" => password}) do
     case Accounts.authenticate(email, password) do
       {:ok, user} ->
-        token = Accounts.generate_jwt(user)
+        token = Guardian.generate_jwt(user)
         json(conn, %{token: token})
 
       {:error, _changeset} ->
         conn
         |> put_status(:unauthorized)
         |> json(%{error: "Invalid Credentials"})
+    end
+  end
+
+  def me(conn, _) do
+    user = Guardian.Plug.current_resource(conn)
+
+    case user do
+      nil ->
+        conn |> put_status(401) |> json(%{error: "Unauthorized"})
+
+      _ ->
+        conn |> put_status(200) |> json(%{user: user})
     end
   end
 
