a7b2f34da1e9d93e6d87cb04f1213f64dbe0e600
Solly <solobarine@gmail.com>
2024-10-29 08:37:09 +0100

    migration: create migration for post

---
+++ backend/priv/repo/migrations/20241023230945_create_posts.exs
@@ -0,0 +1,15 @@
+defmodule Backend.Repo.Migrations.CreatePosts do
+  use Ecto.Migration
+
+  def change do
+    create table(:posts) do
+      add :title, :string
+      add :body, :text
+      add :user_id, references(:users, on_delete: :nothing)
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:posts, [:user_id])
+  end
+end
