779df8faacb4ff4442fcccf45ffa5e9fde9984ee
Solly <solobarine@gmail.com>
2025-01-15 19:21:55 +0100

    Create User Interface

--- client/src/types/auth.ts
+++ client/src/types/auth.ts
@@ -10,3 +10,9 @@ export interface LoginInterface {
   email: string;
   password: string;
 }
+
+export interface User
+  extends Pick<RegisterInterface, "first_name" | "last_name" | "email"> {
+  username: string;
+  bio: string;
+}
