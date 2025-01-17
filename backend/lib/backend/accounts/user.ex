22294265de353cb8dbbbf80aae727400da784cc8
Solly <solobarine@gmail.com>
2025-01-17 14:35:02 +0100

    validate and cast tos

--- backend/lib/backend/accounts/user.ex
+++ backend/lib/backend/accounts/user.ex
@@ -9,6 +9,7 @@ defmodule Backend.Accounts.User do
     field :last_name, :string
     field :email, :string
     field :password_hash, :string
+    field :tos, :boolean
 
     timestamps(type: :utc_datetime)
   end
@@ -16,8 +17,8 @@ defmodule Backend.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:first_name, :last_name, :username, :email, :password_hash])
-    |> validate_required([:first_name, :last_name, :username, :email, :password_hash])
+    |> cast(attrs, [:first_name, :last_name, :username, :email, :password_hash, :tos])
+    |> validate_required([:first_name, :last_name, :email, :password_hash, :tos])
     |> unique_constraint(:email)
     |> put_password_hash
   end
