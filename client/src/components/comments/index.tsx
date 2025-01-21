ef338fb715a1e89bd4d804df16252a6de7a66160
Solly <solobarine@gmail.com>
2025-01-21 21:51:07 +0100

    display data

--- client/src/components/comments/index.tsx
+++ client/src/components/comments/index.tsx
@@ -1,33 +1,35 @@
 import { useState } from "react";
 import NewComment from "./new";
+import { Comment as CommentInterface } from "../../types/comment";
+import { capitalizeText } from "../../utils";
+import { formatDistanceToNow } from "date-fns";
 
-const Comment = () => {
+const Comment = ({ comment }: { comment: CommentInterface }) => {
   const [replyState, setReplyState] = useState(false);
   return (
     <div className="flex items-start gap-3">
       <img src="#" alt="" className="w-8 h-8 rounded-full bg-light" />
-      <div>
+      <div className="grow">
         <div>
           <span className="flex items-center justify-between">
-            <p>Arthur Morgan</p>
+            <p>
+              {capitalizeText(comment.user.first_name)}{" "}
+              {capitalizeText(comment.user.last_name)}
+            </p>
             <span className="flex items-center gap-3">
-              <p>1d</p>
+              <p>{formatDistanceToNow(comment.inserted_at)}</p>
               <button>
                 <i className="bx bx-dots-horizontal-rounded" />
               </button>
             </span>
           </span>
-          <p className="my-2">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
-            nisi eaque quae animi inventore eligendi dolor iusto magni minima?
-            Ullam.
-          </p>
+          <p className="my-2">{comment.text}</p>
           <span className="flex items-center gap-2 text-sm mb-3">
             <button>Like</button>|
             <button onClick={() => setReplyState(true)}>Reply</button>
           </span>
         </div>
-        {replyState && <NewComment />}
+        {replyState && <NewComment comment_id={comment.id} />}
       </div>
     </div>
   );
--- client/src/components/posts/card/index.tsx
+++ client/src/components/posts/card/index.tsx
@@ -1,15 +1,21 @@
+import { formatDistanceToNow } from "date-fns";
+import { Post } from "../../../types/post";
+import { capitalizeText } from "../../../utils";
 import Comment from "../../comments";
 import Create from "../../comments/create";
 
-const Card = () => {
+const Card = ({ post }: { post: Post }) => {
   return (
-    <div className="border dark:border-stone-600 rounded-xl max-w-xl mx-auto shadow-md bg-white dark:bg-stone-700 p-3">
+    <div className="border dark:border-stone-600 rounded-xl max-w-xl w-full mx-auto shadow-md bg-white dark:bg-stone-700 p-3">
       <div className="flex items-start justify-between">
         <div className="flex items-center gap-3">
           <img src="" alt="" className="bg-gray-400 rounded-full w-10 h-10" />
           <span>
-            <p>Solly March</p>
-            <p className="text-sm">2 hours ago</p>
+            <p>
+              {capitalizeText(post.user.first_name)}{" "}
+              {capitalizeText(post.user.last_name)}
+            </p>
+            <p className="text-sm">{formatDistanceToNow(post.inserted_at)}</p>
           </span>
         </div>
         <button>
@@ -17,12 +23,7 @@ const Card = () => {
         </button>
       </div>
       <div className="py-2">
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. In velit
-          libero itaque voluptatum quam hic facere neque nisi error
-          reprehenderit? Eius neque maxime a provident enim quo rem dignissimos
-          placeat?
-        </p>
+        <p>{post.body}</p>
         <img
           src=""
           alt=""
@@ -36,15 +37,26 @@ const Card = () => {
             <i className="bx bxs-heart-circle" />
             <i className="bx bx-meteor" />
           </span>
-          235
+          {post.likes_count}
         </p>
-        <p>9 comments &bull; 3 reposts</p>
+        <p>
+          {post.comments_count}{" "}
+          {post.comments_count !== 1 ? "comments" : "comment"} &bull;{" "}
+          {post.reposts_count} {post.reposts_count !== 1 ? "reposts" : "repost"}
+        </p>
       </span>
       <div className="flex items-center justify-between py-2">
-        <button className="flex items-center gap-1">
-          <i className="bx bx-like" />
-          Like
-        </button>
+        {post.is_liked_by_user ? (
+          <button className="flex items-center gap-1">
+            <i className="bx bx-like" />
+            Unlike
+          </button>
+        ) : (
+          <button className="flex items-center gap-1">
+            <i className="bx bx-like" />
+            Like
+          </button>
+        )}
         <button className="flex items-center gap-1">
           <i className="bx bx-message-square-dots" />
           Comment
@@ -55,10 +67,10 @@ const Card = () => {
         </button>
       </div>
       <hr />
-      <Create />
+      <Create content_id={post.id as number} />
       <div className="grid gap-3">
-        {[1, 2, 3, 4].map((_, index) => (
-          <Comment key={index} />
+        {post.comments.map((comment, index) => (
+          <Comment key={index} comment={comment} />
         ))}
       </div>
     </div>
