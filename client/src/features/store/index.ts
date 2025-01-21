4cc3eca6e14611b64db2fa66c4adee03fc7f72de
Solly <solobarine@gmail.com>
2025-01-21 21:48:40 +0100

    add post reducer to store

--- client/src/features/store/index.ts
+++ client/src/features/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import settings from "../slices/settings";
 import auth from "../slices/auth";
+import posts from "../slices/posts";
 
 const store = configureStore({
   reducer: {
     settings,
     auth,
+    posts,
   },
 });
 
