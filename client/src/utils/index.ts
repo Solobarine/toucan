9cf8079caf7565795f45c33c9e36f3caf9771303
Solly <solobarine@gmail.com>
2025-01-21 21:46:50 +0100

    add util function

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -3,3 +3,5 @@ export const capitalizeText = (text?: string) => {
     ? ""
     : text[0].toUpperCase() + text.slice(1);
 };
+
+export const serverError = () => "Server Error. We are working to resolve this";
