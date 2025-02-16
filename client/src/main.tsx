46a35b66d081eb893e2260b510508d5f29c1d97d
Solly <solobarine@gmail.com>
2025-02-16 20:09:37 +0100

    re-add strict mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,9 +7,9 @@ import "./index.css";
 import store from "./features/store";
 
 createRoot(document.getElementById("root")!).render(
-  //<StrictMode>
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>
-  // </StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
 );
